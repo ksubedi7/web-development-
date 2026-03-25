81f59034f1b0fd60d70224ddf643b7d3c35c546a
ksubedi7 <k.subedi.1123@westcliff.edu>
2026-03-25 09:49:31 +0545

    assignment-4

---
+++ fifth-class/assignment-3/script.js
@@ -0,0 +1,22 @@
+console.log("hello")
+var k=7;
+var s=9;
+var sum = k+s;
+console.log(sum);
+var x = 25;
+var y = 4;
+var remainder = x % y;
+console.log("Remainder:", remainder);
+let a = 4;
+if (true){
+    let a = 5;
+}
+console.log(a);
+
+let a1 = 5;
+let b1 = 6;
+let c1 = 7;
+
+console.log(a1 + b1);
+console.log(a1 + c1);
+console.log(b1 + c1);
